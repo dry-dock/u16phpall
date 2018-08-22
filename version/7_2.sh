549154137f17d35e39853b435216a8c9878dbe2f
Ranjansingh41 <37403043+Ranjansingh41@users.noreply.github.com>
2018-08-22 11:14:39 +0530

    Update 7_2.sh

--- version/7_2.sh
+++ version/7_2.sh
@@ -1,19 +1,21 @@
 #!/bin/bash -e
 
-#Build PHP 7.2.7
-echo "============ Building PHP 7.2.7 =============="
-PHP_BUILD_CONFIGURE_OPTS="--with-bz2 --enable-intl" php-build -i development 7.2.7 $HOME/.phpenv/versions/7.2
+PHP7_2_VERSION="7.2.9"
+echo "============ Building $PHP7.2_VERSION  =============="
 
-# Setting phpenv to 7.2.7
+PHP_BUILD_CONFIGURE_OPTS="--with-bz2 --enable-intl" php-build -i development "$PHP7_2_VERSION" $HOME/.phpenv/versions/7.2
+
+# Setting phpenv to PHP7.2_VERSION
 echo "============ Setting phpenv to 7.2 ============"
 phpenv rehash
 phpenv global 7.2
 
 # Install phpunit
+PHPUNIT_VERSION="7.3.0"
 echo "============ Installing PHPUnit ============="
-wget -nv https://phar.phpunit.de/phpunit-7.2.6.phar
-chmod +x phpunit-7.2.6.phar
-mv phpunit-7.2.6.phar $HOME/.phpenv/versions/7.2/bin/phpunit
+wget -nv https://phar.phpunit.de/phpunit-"$PHPUNIT_VERSION".phar
+chmod +x phpunit-"$PHPUNIT_VERSION".phar
+mv phpunit-"$PHPUNIT_VERSION".phar $HOME/.phpenv/versions/7.2/bin/phpunit
 
 # Install Composer
 echo "============ Installing Composer ============"
@@ -21,6 +23,7 @@ curl -sS http://getcomposer.org/installer | php
 chmod +x composer.phar
 mv composer.phar $HOME/.phpenv/versions/7.2/bin/composer
 
+sudo apt-get install php7.2-ldap
 #install pickle
 cd /tmp/pickle
 $HOME/.phpenv/versions/7.2/bin/composer install --no-dev
@@ -37,5 +40,4 @@ printf '\n' | bin/pickle install --with-configure-options=/tmp/pickle-mongodb-op
 rm /tmp/pickle-mongodb-opts
 
 cd /
-
 
